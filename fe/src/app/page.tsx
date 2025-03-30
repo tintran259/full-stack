13a8af75b9d3bd9e13027301cf5551429ded69d4
tintran <tin.trannhu@digicommercegroup.com>
2025-03-30 18:21:06 +0700

    [Setup] FE

---
+++ fe/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <>
+      <Button type="primary" className="mt-6 ml-6">
+        Click
+      </Button>
+    </>
+  );
+}
